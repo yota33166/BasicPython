aa4e4d60f4a3dffc9ba92f8554b9760c618d1ef6
Yota Nagai <y.naga.23.7.1110@4nonome.com>
2024-02-27 18:45:59 +0900

    add: 問題回答

--- euclid.py
+++ euclid.py
@@ -2,3 +2,18 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+a, b = map(int, (a, b))
+
+
+def euclid_gcd(a: int, b: int):
+    if a < b:
+        a, b = b, a
+
+    remainder = a % b
+    if remainder == 0:
+        return b
+    else:
+        return euclid_gcd(b, remainder)
+
+
+print("最大公約数は", euclid_gcd(a, b))
--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,23 @@
 a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
+
 # TODO
+def is_prime(n: int):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+
+    for i in range(2, int(n ** (1 / 2)) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+for num in map(int, (a, b)):
+    if is_prime(num):
+        print(num, "は素数です")
+    else:
+        print(num, "は素数ではありません")
