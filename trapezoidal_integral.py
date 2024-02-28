05cd72fe74c7318ea86bf5069f406778845875c2
Yota Nagai <y.naga.23.7.1110@4nonome.com>
2024-02-28 22:01:45 +0900

    fix: 関数の初期値を設定

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -6,7 +6,7 @@ from math import pi, sin, sqrt, exp
 # -----------
 
 
-def trapezoidal_integral(f, a: int | float, b: int | float, n: int) -> float:
+def trapezoidal_integral(f, a = 0, b = 1, n = 100) -> float:
     sum = 0
     h = (b - a) / n
     for k in range(1, n + 1):
@@ -24,5 +24,5 @@ def func3(x: int | float):
 
 
 print(f"S = {trapezoidal_integral(sin, 0, pi/2, 50)}")
-print(f"S = {trapezoidal_integral(func2, 0, 1, 100)}")
+print(f"S = {trapezoidal_integral(func2)}")
 print(f"S = {trapezoidal_integral(func3, -100, 100, 1000)}")
